b4fffab082828e8d43f6d86529d5dba467e7dc92
heshanu <heshanu97@gmail.com>
2023-04-19 23:26:30 +0530

    Added chatengine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
 function App() {
-  return (  
-    <div   className="App">
-      < header className="App-header">
-       <h1>Hello</h1>
-      </header>
+  return (
+    <div className="App">
+
+      <ChatEngine height="100vh"
+        projectID="523bcdd7-70b6-49c5-ba64-157f3994e981"
+        userName="heshan"
+        userSecret="qwerty" />
     </div>
   );
 }
