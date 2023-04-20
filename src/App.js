0ffcef8f39ce9dcaf76c1783c68eed6d4c345f04
heshanu <heshanu97@gmail.com>
2023-04-20 23:21:07 +0530

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import React from 'react'
 import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed  from './compoents/ChatFeed.jsx';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
       <ChatEngine height="100vh"
         projectID="523bcdd7-70b6-49c5-ba64-157f3994e981"
         userName="heshan"
-        userSecret="qwerty" />
+        userSecret="qwerty"
+        renderChatFeed={(chatAppProps) => { <ChatFeed {...chatAppProps} /> }}
+        onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+      />
     </div>
   );
 }
